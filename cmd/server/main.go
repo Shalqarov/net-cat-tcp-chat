f7597d23e76990f791e7f9e0e6a6e5ce62ed94ef
Темирлан Шалкаров <Temirlan200312@gmail.com>
2022-08-11 14:41:54 +0600

    var rename

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,12 +18,12 @@ var (
 )
 
 const (
-	greetings = "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]: "
+	WELCOME_MSG = "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]: "
 
-	timeFormat = "2006-01-02 15:04:05"
+	TIME_FORMAT = "2006-01-02 15:04:05"
 )
 
-type Connect struct {
+type Server struct {
 	sync.Mutex
 	users *sync.Map
 }
@@ -46,7 +46,7 @@ func startServer() {
 	defer listener.Close()
 
 	log.Printf("Listening for connections on %s", listener.Addr().String())
-	connections := Connect{
+	server := Server{
 		users: &sync.Map{},
 	}
 	for {
@@ -56,15 +56,15 @@ func startServer() {
 			conn.Close()
 			continue
 		}
-		connections.register(conn)
-		go connections.handleConnection(conn)
+		server.register(conn)
+		go server.handleConnection(conn)
 	}
 }
 
-func (c *Connect) handleConnection(conn net.Conn) {
+func (s *Server) handleConnection(conn net.Conn) {
 	defer func() {
 		conn.Close()
-		c.users.Delete(conn)
+		s.users.Delete(conn)
 	}()
 
 	for {
@@ -73,39 +73,38 @@ func (c *Connect) handleConnection(conn net.Conn) {
 			return
 		}
 		if userInput == "\n" {
-			fmt.Fprint(conn, c.message(time.Now(), conn))
+			fmt.Fprint(conn, s.message(conn))
 			continue
 		}
-		c.users.Range(func(key, value interface{}) bool {
+		s.users.Range(func(key, value interface{}) bool {
 			if _, ok := value.(string); ok && key.(net.Conn) != conn {
-				c.sendMessage(key.(net.Conn), userInput)
+				s.sendMessage(key.(net.Conn), userInput)
 			}
 			return true
 		})
-		fmt.Fprint(conn, c.message(time.Now(), conn))
+		fmt.Fprint(conn, s.message(conn))
 	}
 }
 
-func (c *Connect) message(now time.Time, conn net.Conn) string {
-	username, _ := c.users.Load(conn)
-	return "[" + now.Format(timeFormat) + "]" + "[" + username.(string) + "]:"
+func (s *Server) message(conn net.Conn) string {
+	username, _ := s.users.Load(conn)
+	return "[" + time.Now().Format(TIME_FORMAT) + "]" + "[" + username.(string) + "]:"
 }
 
-func (c *Connect) sendMessage(conn net.Conn, input string) {
+func (s *Server) sendMessage(conn net.Conn, input string) {
 	fmt.Fprintln(conn)
-	fmt.Fprint(conn, c.message(time.Now(), conn))
+	fmt.Fprint(conn, s.message(conn))
 	fmt.Fprint(conn, input)
-	fmt.Fprint(conn, c.message(time.Now(), conn))
+	fmt.Fprint(conn, s.message(conn))
 }
 
-func (c *Connect) register(conn net.Conn) {
-	username := ""
-	fmt.Fprint(conn, greetings)
+func (s *Server) register(conn net.Conn) {
+	fmt.Fprint(conn, WELCOME_MSG)
 	username, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		log.Println(err.Error())
 		return
 	}
-	c.users.Store(conn, strings.TrimRight(username, "\r\n"))
-	fmt.Fprint(conn, c.message(time.Now(), conn))
+	s.users.Store(conn, strings.TrimRight(username, "\r\n"))
+	fmt.Fprint(conn, s.message(conn))
 }
